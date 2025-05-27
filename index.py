5d07093d6751a37c33506343d696d8f2a2688f7d
Adnan00707 <adnansoni2020@gmail.com>
2025-05-27 21:20:56 +0400

    Added stopwords locally

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import joblib
 import re
+import nltk
+nltk.data.path.append("nltk_data")
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
